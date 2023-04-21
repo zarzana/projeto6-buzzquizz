788170c4bc5e1c7990a892154ea40739878752a3
JeanPiton <jean.orpiton@gmail.com>
2023-04-20 23:09:41 -0300

    basico+perguntas

--- tela3.js
+++ tela3.js
@@ -1,7 +1,27 @@
+const obj = {};
 
+function checkImage(url) {
+    const img = new Image();
+    img.src = url;
+    return new Promise((resolve) => {
+    img.onerror = () => resolve(false);
+    img.onload = () => resolve(true);});
+}
+
+function addCreation(){
+    const areaCreate = document.querySelector(".area");
+    areaCreate.innerHTML=`<div class="create">
+        <input type="text" class="tituloCreate" id="" placeholder="Titulo do seu Quizz">
+        <input type="text" class="urlCreate" id="" placeholder="URL da imagem do seu quizz">
+        <input type="text" class="perguntasCreate" id="" placeholder="Quantidade de perguntas do quizz">
+        <input type="text" class="niveisCreate" id="" placeholder="Quantidade de níveis do quizz">
+    </div>
+    <button onclick="Questions()">Prosseguir pra criar perguntas</button>`;
+}
 
 function addQuestions(numero){
     const areaQuestions = document.querySelector(".area");
+    areaQuestions.innerHTML="";
     for(let i=0;i<numero;i++){
         areaQuestions.innerHTML+=`<div class="pergunta" onclick="selectOption(this)">
         <div class="titulo"><p>Pergunta ${i+1}</p><ion-icon name="create-outline"></ion-icon></div>
@@ -25,11 +45,13 @@ function addQuestions(numero){
         </div>
     </div>`;
     }
-    areaQuestions.innerHTML+=`<button>Prosseguir pra criar níveis</button>`;
+    areaQuestions.innerHTML+=`<button onclick="Niveis()">Prosseguir pra criar níveis</button>`;
+    selectOption(document.querySelector(".pergunta"));
 }
 
 function addLevels(numero){
     const areaLevels = document.querySelector(".area");
+    areaLevels.innerHTML="";
     for(let i=0;i<numero;i++){
         areaLevels.innerHTML+=`<div class="nivel" onclick="selectOption(this)">
         <div class="titulo"><p>Nivel ${i+1}</p><ion-icon name="create-outline"></ion-icon></div>
@@ -41,7 +63,19 @@ function addLevels(numero){
             </div>
         </div>`;
     }
-    areaLevels.innerHTML+=`<button>Finalizar Quizz</button>`;
+    areaLevels.innerHTML+=`<button onclick="Success()">Finalizar Quizz</button>`;
+    selectOption(document.querySelector(".nivel"));
+}
+
+function addSuccess(frase,url){
+    const areaSuccess = document.querySelector(".area");
+    areaSuccess.innerHTML="";
+        areaSuccess.innerHTML+=`<div class="quizz">
+        <img class="img_quiz" src="${url}">
+        <div class="text_quizz"><p>${frase}</p></div>
+        </div>
+        <button>Acessar Quizz</button>
+        <button class="button2">Voltar para home</button>`;
 }
 
 function selectOption(element){
@@ -58,3 +92,100 @@ function selectOption(element){
     }
 }
 
+function Create(){
+    document.body.innerHTML=`<div class="fixed_top_bar">
+    <h1>BuzzQuizz</h1>
+    </div>
+    <p class="criarPTitulo">Comece pelo começo</p>
+    <div class="area">
+    </div>`;
+    addCreation();
+}
+
+async function Questions(){
+    const titulo = document.querySelector(".tituloCreate");
+    const imagem = document.querySelector(".urlCreate");
+    const perguntas = document.querySelector(".perguntasCreate");
+    const niveis = document.querySelector(".niveisCreate");
+    if((titulo.value.length>=20&&titulo.value.length<=65)&&
+    await checkImage(imagem.value)&&
+    (Number(perguntas.value)>=3)&&
+    (Number(niveis.value)>=2)){
+        obj["title"] = `${titulo.value}`;
+        obj["image"] = `${imagem.value}`;
+        obj["questions"] = [];
+        obj["levels"] = [];
+        for(let i=0;i<perguntas.value;i++){
+            obj["questions"][i] = {};
+        }
+        for(let i=0;i<niveis.value;i++){
+            obj["levels"][i] = {};
+        }
+        document.querySelector(".criarPTitulo").innerHTML="Crie suas perguntas";
+        addQuestions(obj["questions"].length);
+    }
+    else{
+        alert("Preencha os dados corretamente");
+    }
+}
+
+async function Niveis(){
+    let rivazio = 0;
+    const respIfiltro = [];
+    const RegExp = /^#[0-9A-F]{6}$/i;
+    let textoP=[],corP=[],respcP=[],respURL=[],respI=[];
+    const perguntas = document.querySelectorAll(".pergunta");
+    perguntas.forEach(p=>textoP.push(p.querySelector(".textPergunta")));
+    const textoPfiltro=textoP.filter(x => x.value.length<20);
+    perguntas.forEach(p=>corP.push(p.querySelector(".corPergunta")));
+    const corPfiltro=corP.filter(x => !RegExp.test(x.value));
+    perguntas.forEach(p=>respcP.push(p.querySelector(".respostaCorreta")));
+    const respcPfiltro=respcP.filter(x => x.value.length==0);
+    perguntas.forEach(p=>respURL.push(p.querySelector(".urlResposta")));
+    const respURLfiltro=respURL.filter(x => !checkImage(x.value));
+    perguntas.forEach(p=>respI.push(p.querySelectorAll(".respostaIncorreta")));
+    for(let i=0;i<respI.length;i++){
+        respIfiltro.push([]);
+        let v = 0;
+        for(let c=0;c<respI[i].length;c++){
+            if(respI[i][c].value.length!=0){
+                respIfiltro[i].push(respI[i][c]);
+            }else{
+                v++;
+            }
+            if(v==respI[i].length){
+                rivazio++;
+            }
+        }
+    }
+    if(textoPfiltro.length==0&&
+        corPfiltro.length==0&&
+        respcPfiltro.length==0&&
+        respURLfiltro.length==0&&
+        rivazio==0){
+        for(let i=0;i<obj["questions"].length;i++){
+            obj["questions"][i]['title'] = textoP[i].value;
+            obj["questions"][i]['color'] = corP[i].value;
+            obj["questions"][i]['answers'] = [{}];
+            obj["questions"][i]['answers'][0]['text'] = respcP[0].value
+            obj["questions"][i]['answers'][0]['image'] = respURL[0].value
+            obj["questions"][i]['answers'][0]['isCorrectAnswer'] = true;
+            for(let c=0;c<respIfiltro[i].length;c++){
+                obj["questions"][i]['answers'][c+1]={};
+                obj["questions"][i]['answers'][c+1]['text'] = respIfiltro[i][c].value
+                obj["questions"][i]['answers'][c+1]['image'] = respIfiltro[i][c].nextElementSibling.value
+                obj["questions"][i]['answers'][c+1]['isCorrectAnswer'] = false;
+            }
+        }
+        document.querySelector(".criarPTitulo").innerHTML="Agora, decida os níveis";
+        addLevels(qtdNivel);
+    }else{
+        alert("Preencha os dados corretamente");
+        console.log(textoPfiltro.length,corPfiltro.length,respcPfiltro.length,respURLfiltro.length,rivazio);
+    }
+}
+
+function Success(){
+    document.querySelector(".criarPTitulo").innerHTML="Seu quizz está pronto!";
+    addSuccess(titulo,url);
+}
